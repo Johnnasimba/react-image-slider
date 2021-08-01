b4bd6ebbf481c94e6dc3cef43123b321e93552f9
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-01 15:07:12 +0200

    Adding carousel to slides

--- src/component/image-slider/image-slider.component.jsx
+++ src/component/image-slider/image-slider.component.jsx
@@ -1,13 +1,29 @@
 
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 import './image-slider.styles.css';
 
 
-const ImageSlider = ({
-    images=[]
-}) => {
-    console.log(images)
+const ImageSlider = ({ images=[], autoPlayTime = 5000, ...props}) => {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    // Change slide
+    const nextSlide = (slideIndex = currentSlide + 1) => {
+        const newSlideIndex = slideIndex >= images.length?0 : slideIndex;
+        setCurrentSlide(newSlideIndex)
+    }
+
+    useEffect(() => {
+        // move to next slide on timeout
+
+        const timer = setTimeout(()=>{
+            nextSlide()
+        }, autoPlayTime)
+        
+        return () => clearTimeout(timer)
+    })
+
     return (
         <div>
             <div className="image-slider-wrapper">               
@@ -16,10 +32,10 @@ const ImageSlider = ({
                             <div 
                                 className="slide-image"
                                 key={index}
-                                style={{backgroundImage: "green"}}
-
-                            
-                            >
+                                style={{
+                                    backgroundImage: "green",
+                                    marginLeft: index === 0 ? `-${currentSlide * 100}%`: undefined
+                                }}>
                                 <img src={imageUrl} alt="Outside work" />
                             </div>
                         ))

13f09a25cfa3e3be454bcaf062a261b0c8818eee
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-01 14:15:51 +0200

    Mapping through images

--- src/component/image-slider/image-slider.component.jsx
+++ src/component/image-slider/image-slider.component.jsx
@@ -4,14 +4,30 @@ import React from 'react';
 import './image-slider.styles.css';
 
 
-const ImageSlider = () => {
+const ImageSlider = ({
+    images=[]
+}) => {
+    console.log(images)
     return (
         <div>
             <div className="image-slider-wrapper">               
-                    <div className="slide-image">
-                        <img src="./images/school-life.jpg" alt="" />
-                    </div>
+                    {
+                        images.map((imageUrl, index) => (
+                            <div 
+                                className="slide-image"
+                                key={index}
+                                style={{backgroundImage: "green"}}
+
+                            
+                            >
+                                <img src={imageUrl} alt="Outside work" />
+                            </div>
+                        ))
+                    }
             </div>
+
+
+
             <div className="navigation-dots">
                 <div className="single-dot active"></div>
                 <div className="single-dot"></div>

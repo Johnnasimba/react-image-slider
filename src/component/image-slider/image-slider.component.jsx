213c746f80ab954a51e8f2e6961636b507cf09c3
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-01 13:33:12 +0200

    Re-designing the ui

--- src/component/image-slider/image-slider.component.jsx
+++ src/component/image-slider/image-slider.component.jsx
@@ -7,21 +7,12 @@ import './image-slider.styles.css';
 const ImageSlider = () => {
     return (
         <div>
-            <div className="image-slider">
-                <div className="image-slider-left-button">
-                    <i class="fas fa-arrow-circle-left fa-3x"></i>
-                </div>
-                <div className="slides-container">
+            <div className="image-slider-wrapper">               
                     <div className="slide-image">
-                        <img src="./Images/me-and-my-father.jpg" alt="" />
+                        <img src="./images/school-life.jpg" alt="" />
                     </div>
-                </div>
-                <div className="image-slider-right-button">
-                    <i class="fas fa-arrow-circle-right fa-3x"></i>
-                </div>
-
             </div>
-            <div class="navigation-dots">
+            <div className="navigation-dots">
                 <div className="single-dot active"></div>
                 <div className="single-dot"></div>
                 <div className="single-dot"></div>
